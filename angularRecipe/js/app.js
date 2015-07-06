56011caccb75afadc98f2c7f1a44c3f248ff2096
d.chauquoc <d.chauquoc@gmail.com>
2015-07-06 15:41:03 +0700

    Update

--- angularRecipe/js/app.js
+++ angularRecipe/js/app.js
@@ -7,15 +7,38 @@ app.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
 	// Home
 	.when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+
+	
 	// Pages
 	.when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
-	.when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
+	/*.when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
 	.when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
-	.when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
+	.when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})*/
 	.when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
+
+		// jQuery
+		.when("/jquery/home", {templateUrl: "partials/jquery/home.html", controller: "PageCtrl"})
+
+		// Bootstrap
+		.when("/bootstrap/tutorial", {templateUrl: "partials/bootstrap/tutorial.html", controller: "PageCtrl"})
+		.when("/bootstrap/getStarted", {templateUrl: "partials/bootstrap/getStarted.html", controller: "PageCtrl"})
+		.when("/bootstrap/gridBasic", {templateUrl: "partials/bootstrap/gridBasic.html", controller: "PageCtrl"})
+		.when("/bootstrap/typography", {templateUrl: "partials/bootstrap/typography.html", controller: "PageCtrl"})
+
+		/*
+		
+		
+		.when("/bootstrap/tutorial", {templateUrl: "partials/bootstrap/tutorial.html", controller: "PageCtrl"})*/
+
+		// Angular
+		.when("/angular/home", {templateUrl: "partials/angular/home.html", controller: "PageCtrl"})
+
+	
 	// Blog
 	.when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
 	.when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
+	
+
 	// else 404
 	.otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
